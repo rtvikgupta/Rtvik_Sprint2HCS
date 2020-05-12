9a5019a3e4a1223faaf8104696a6488634f5ecf9
Rtvik Gupta <35890433+rtvikgupta@users.noreply.github.com>
2020-05-12 08:22:51 +0530

    Update IUserDao.java

--- UserManagement/src/main/java/com/capgemini/rtvik/healthcare/dao/IUserDao.java
+++ UserManagement/src/main/java/com/capgemini/rtvik/healthcare/dao/IUserDao.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.capgemini.rtvik.healthcare.entities.User;
 
 public interface IUserDao extends JpaRepository<User, String> {
-
+	
+	boolean findByUserEmail(String userEmail);
+  
 }
