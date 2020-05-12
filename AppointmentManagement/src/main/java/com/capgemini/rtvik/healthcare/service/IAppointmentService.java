667550f7c6b8006d617ce3dc201300b5d90451e9
Rtvik Gupta <35890433+rtvikgupta@users.noreply.github.com>
2020-05-12 08:19:37 +0530

    Update IAppointmentService.java

--- AppointmentManagement/src/main/java/com/capgemini/rtvik/healthcare/service/IAppointmentService.java
+++ AppointmentManagement/src/main/java/com/capgemini/rtvik/healthcare/service/IAppointmentService.java
@@ -1,15 +1,20 @@
 package com.capgemini.rtvik.healthcare.service;
 
 import java.math.BigInteger;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.capgemini.rtvik.healthcare.entities.Appointment;
 
 public interface IAppointmentService {
 	
-	public Appointment approveAppointment(Appointment app);
-	public Appointment saveAppointment(Appointment app);
-	public Appointment findById(BigInteger appointmentId);
-	public List<Appointment> findByCenter(String centerId);
-	public List<Appointment> findByUser(String userId);
+	boolean approveAppointment(Appointment app);
+	
+	String makeAppointment(String userId, String centerId, String testId, LocalDateTime dateTime);
+	
+	Appointment findById(BigInteger appointmentId);
+	
+	List<Appointment> findByCenter(String centerId);
+	
+	List<Appointment> findByUser(String userId);
 }
